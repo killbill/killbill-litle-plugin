2f281316c3ea42064e201909fc12ddda18e49d29
Pierre-Alexandre Meyer <pierre@ning.com>
2013-05-08 17:31:07 -0700

    release.sh: update release script
    See https://github.com/killbill/killbill-litle-plugin/wiki/Killbill-Litle-plugin-Release.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- release.sh
+++ release.sh
@@ -1,4 +1,20 @@
+set -e
+
 VERSION=`grep -E '<version>([0-9]+\.[0-9]+\.[0-9]+)</version>' pom.xml | sed 's/[\t \n]*<version>\(.*\)<\/version>[\t \n]*/\1/'`
+if [ "$VERSION" != "$(cat $PWD/VERSION)" ]; then
+  echo "Unable to release: make sure the versions in pom.xml and VERSION match"
+  exit 1
+fi
+
+echo "Cleaning up"
+rake killbill:clean ; rake build
+
+echo "Pushing the gem to Rubygems"
+rake release
+
+echo "Building artifact"
+rake killbill:package
+
 ARTIFACT="$PWD/pkg/killbill-litle-$VERSION.tar.gz"
 echo "Pushing $ARTIFACT to Maven Central"
 mvn gpg:sign-and-deploy-file \
