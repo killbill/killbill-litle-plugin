af879fc26321d6cbf7368a2e7d21a7c528d32e87
Pierre-Alexandre Meyer <pierre@ning.com>
2013-05-23 17:13:10 -0700

    api: handle UUID objects from Kill Bill
    Until we have custom converters, we need to call .to_s on UUID model
    objects.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@ning.com>

--- lib/litle/api.rb
+++ lib/litle/api.rb
@@ -17,12 +17,12 @@ module Killbill::Litle
     def process_payment(kb_account_id, kb_payment_id, kb_payment_method_id, amount_in_cents, currency, call_context, options = {})
       # If the payment was already made, just return the status
       # TODO Should we set the Litle Id field to check for dups (https://www.litle.com/mc-secure/DupeChecking_V1.2.pdf)?
-      litle_transaction = LitleTransaction.from_kb_payment_id(kb_payment_id) rescue nil
+      litle_transaction = LitleTransaction.from_kb_payment_id(kb_payment_id.to_s) rescue nil
       return litle_transaction.litle_response.to_payment_response unless litle_transaction.nil?
 
       # Required argument
       # Note! The field is limited to 25 chars, so we convert the UUID (in hex) to base64
-      options[:order_id] ||= Utils.compact_uuid kb_payment_id
+      options[:order_id] ||= Utils.compact_uuid kb_payment_id.to_s
 
       # Set a default report group
       options[:merchant] ||= report_group_for_currency(currency)
@@ -37,7 +37,7 @@ module Killbill::Litle
     end
 
     def process_refund(kb_account_id, kb_payment_id, amount_in_cents, currency, call_context, options = {})
-      litle_transaction = LitleTransaction.find_candidate_transaction_for_refund(kb_payment_id, amount_in_cents)
+      litle_transaction = LitleTransaction.find_candidate_transaction_for_refund(kb_payment_id.to_s, amount_in_cents)
 
       # Set a default report group
       options[:merchant] ||= report_group_for_currency(currency)
@@ -53,7 +53,7 @@ module Killbill::Litle
       # We assume the payment is immutable in Litle and only look at our tables since there
       # doesn't seem to be a Litle API to fetch details for a given transaction.
       # TODO How can we support Authorization/Sale Recycling?
-      litle_transaction = LitleTransaction.from_kb_payment_id(kb_payment_id)
+      litle_transaction = LitleTransaction.from_kb_payment_id(kb_payment_id.to_s)
 
       litle_transaction.litle_response.to_payment_response
     end
@@ -68,22 +68,22 @@ module Killbill::Litle
       response = save_response_and_transaction litle_response, :add_payment_method
 
       if response.success
-        LitlePaymentMethod.create :kb_account_id => kb_account_id, :kb_payment_method_id => kb_payment_method_id, :litle_token => response.litle_token
+        LitlePaymentMethod.create :kb_account_id => kb_account_id.to_s, :kb_payment_method_id => kb_payment_method_id.to_s, :litle_token => response.litle_token
       else
         raise response.message
       end
     end
 
     def delete_payment_method(kb_account_id, kb_payment_method_id, call_context, options = {})
-      LitlePaymentMethod.mark_as_deleted! kb_payment_method_id
+      LitlePaymentMethod.mark_as_deleted! kb_payment_method_id.to_s
     end
 
     def get_payment_method_detail(kb_account_id, kb_payment_method_id, tenant_context, options = {})
-      LitlePaymentMethod.from_kb_payment_method_id(kb_payment_method_id).to_payment_method_response
+      LitlePaymentMethod.from_kb_payment_method_id(kb_payment_method_id.to_s).to_payment_method_response
     end
 
     def get_payment_methods(kb_account_id, refresh_from_gateway, call_context, options = {})
-      LitlePaymentMethod.from_kb_account_id(kb_account_id).collect { |pm| pm.to_payment_method_response }
+      LitlePaymentMethod.from_kb_account_id(kb_account_id.to_s).collect { |pm| pm.to_payment_method_response }
     end
 
     private
@@ -101,19 +101,19 @@ module Killbill::Litle
     end
 
     def get_token(kb_payment_method_id)
-      LitlePaymentMethod.from_kb_payment_method_id(kb_payment_method_id).litle_token
+      LitlePaymentMethod.from_kb_payment_method_id(kb_payment_method_id.to_s).litle_token
     end
 
     def save_response_and_transaction(litle_response, api_call, kb_payment_id=nil, amount_in_cents=0)
       @logger.warn "Unsuccessful #{api_call}: #{litle_response.message}" unless litle_response.success?
 
       # Save the response to our logs
-      response = LitleResponse.from_response(api_call, kb_payment_id, litle_response)
+      response = LitleResponse.from_response(api_call, kb_payment_id.to_s, litle_response)
       response.save!
 
       if response.success and !kb_payment_id.blank? and !response.litle_txn_id.blank?
         # Record the transaction
-        transaction = response.create_litle_transaction!(:amount_in_cents => amount_in_cents, :api_call => api_call, :kb_payment_id => kb_payment_id, :litle_txn_id => response.litle_txn_id)
+        transaction = response.create_litle_transaction!(:amount_in_cents => amount_in_cents, :api_call => api_call, :kb_payment_id => kb_payment_id.to_s, :litle_txn_id => response.litle_txn_id)
         @logger.debug "Recorded transaction: #{transaction.inspect}"
       end
       response
