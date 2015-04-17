f5bbfd52b8d657a72267f2d782df37b750649b1a
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-04-17 04:51:10 -0700

    paypage: add support for multi-tenancy
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- lib/litle/application.rb
+++ lib/litle/application.rb
@@ -27,26 +27,45 @@ get '/plugins/killbill-litle/form', :provides => 'html' do
   kb_account_id = request.GET['kb_account_id']
   required_parameter! :kb_account_id, kb_account_id
 
-  secure_page_url = config[:litle][:secure_page_url]
-  required_parameter! :secure_page_url, secure_page_url, 'is not configured'
+  kb_tenant_id = request.GET['kb_tenant_id']
+  kb_tenant = request.env['killbill_tenant']
+  kb_tenant_id ||= kb_tenant.id.to_s unless kb_tenant.nil?
 
-  # Allow currency override if needed
   currency = request.GET['currency'] || plugin.get_currency(kb_account_id)
-  paypage_id = config[:litle][:paypage_id][currency.to_sym]
+
+  tenant_config = config(kb_tenant_id)
+  litle_config = tenant_config[:litle].is_a?(Array) ? (tenant_config[:litle].find { |c| c[:account_id] == currency }) : tenant_config[:litle]
+
+  paypage_id = litle_config[:paypage_id]
   required_parameter! :paypage_id, paypage_id, "is not configured for currency #{currency.to_sym}"
+
+  secure_page_url = litle_config[:secure_page_url]
+  required_parameter! :secure_page_url, secure_page_url, 'is not configured'
 
   locals = {
       :currency => currency,
       :secure_page_url => secure_page_url,
       :paypage_id => paypage_id,
       :kb_account_id => kb_account_id,
+      :kb_tenant_id => kb_tenant_id,
       :merchant_txn_id => request.GET['merchant_txn_id'] || '1',
       :order_id => request.GET['order_id'] || '1',
       :report_group => request.GET['report_group'] || 'Default Report Group',
-      :success_page => params[:successPage],
+      :success_page => params[:successPage] || '/plugins/killbill-litle',
       :failure_page => params[:failurePage]
   }
   erb :paypage, :locals => locals
+end
+
+# This is mainly for testing. Your application should redirect from the PayPage checkout above
+# to a custom endpoint where you call the Kill Bill add payment method JAX-RS API.
+# If you really want to use this endpoint, you'll have to call the Kill Bill refresh payment methods API
+# to get a Kill Bill payment method id assigned.
+post '/plugins/killbill-litle' do
+  pm = plugin.add_payment_method(params)
+
+  status 201
+  redirect '/plugins/killbill-litle/1.0/pms/' + pm.id.to_s
 end
 
 # curl -v http://127.0.0.1:9292/plugins/killbill-litle/1.0/pms/1
--- lib/litle/private_api.rb
+++ lib/litle/private_api.rb
@@ -9,6 +9,34 @@ module Killbill #:nodoc:
               session)
       end
 
+      def add_payment_method(params)
+        payment_processor_account_id = params[:payment_processor_account_id] || :default
+
+        exp_month, exp_year = (params[:exp_date] || '').split('/')
+
+        # TODO Save response row (to keep the response_litle_txn_id)
+
+        # Create the payment method (not associated with a Kill Bill payment method yet)
+        Killbill::Litle::LitlePaymentMethod.create!(:kb_account_id        => params[:kb_account_id],
+                                                    :kb_payment_method_id => nil,
+                                                    :kb_tenant_id         => params[:kb_tenant_id],
+                                                    :token                => params[:response_paypage_registration_id],
+                                                    :cc_first_name        => params[:first_name],
+                                                    :cc_last_name         => params[:last_name],
+                                                    :cc_type              => nil,
+                                                    :cc_exp_month         => exp_month,
+                                                    :cc_exp_year          => exp_year,
+                                                    :cc_last_4            => params[:cc_num],
+                                                    :address1             => nil,
+                                                    :address2             => nil,
+                                                    :city                 => nil,
+                                                    :state                => nil,
+                                                    :zip                  => nil,
+                                                    :country              => nil,
+                                                    :created_at           => Time.now.utc,
+                                                    :updated_at           => Time.now.utc)
+      end
+
       def get_currency(kb_account_id)
         account = kb_apis.get_account_by_id(kb_account_id)
         account.currency
